4baea37d7f5648d7bff37e00fecbc85aa84cc088
David Lee <david@lee269.co.uk>
2017-12-28 17:07:32 +0000

    Mor packrat playing

--- R/refdata.R
+++ R/refdata.R
@@ -1,25 +1,22 @@
+library(jsonlite)
+
+
 # Read in reference tables from Comtrade
 # Reporter countries table - reporters
 
 string <- "http://comtrade.un.org/data/cache/reporterAreas.json"
-reporters <- fromJSON(file=string)
-reporters <- as.data.frame(t(sapply(reporters$results,rbind)))
+reporters <- fromJSON(txt = string)
+reporters <- as.data.frame(sapply(reporters$results,rbind))
 names(reporters) <- c("id", "name")
 reporters$id <- as.character(reporters$id)
 reporters$name <- as.character(reporters$name)
 
-if(dbExistsTable(comtrade, "reporters")) {
-  dbRemoveTable(comtrade, "reporters")
-  dbWriteTable(comtrade,'reporters', reporters, row.names = FALSE)
-} else {
-  dbWriteTable(comtrade,'reporters', reporters, row.names = FALSE)
-}
 
 # Partner countries table - partners  
 
 string <- "http://comtrade.un.org/data/cache/partnerAreas.json"
-partners <- fromJSON(file=string)
-partners <- as.data.frame(t(sapply(partners$results,rbind)))
+partners <- fromJSON(txt=string)
+partners <- as.data.frame(sapply(partners$results,rbind))
 names(partners) <- c("id", "name")
 partners$id <- as.character(partners$id)
 partners$name <- as.character(partners$name)
@@ -29,8 +26,8 @@ partners$name <- as.character(partners$name)
 # Comcodes table - hscodes
 
 string <- "http://comtrade.un.org/data/cache/classificationHS.json"
-hscodes <- fromJSON(file = string)
-hscodes <- as.data.frame(t(sapply(hscodes$results, rbind)))
+hscodes <- fromJSON(txt = string)
+hscodes <- as.data.frame(sapply(hscodes$results, rbind))
 names(hscodes) <- c("id", "text", "parent")
 hscodes$id <- as.character(hscodes$id)
 hscodes$text <- as.character(hscodes$text)

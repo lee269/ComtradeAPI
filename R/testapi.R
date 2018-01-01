2869cbb2056957338c2d88075f14016227c90596
David Lee <david@lee269.co.uk>
2018-01-01 15:56:06 +0000

    Basic sankey added

--- R/testapi.R
+++ R/testapi.R
@@ -1,12 +1,13 @@
 library(here)
 library(dplyr)
 library(ggplot2)
+library(networkD3)
 
 source(here("R", "get_comtrade.R"))
 source(here("R", "utils.R"))
 
-reporters <- read.csv(here("data", "reporters.csv"), stringsAsFactors = TRUE, header = TRUE)
-partners <- read.csv(here("data", "partners.csv"), stringsAsFactors = TRUE, header = TRUE)
+reporters <- read.csv(here("data", "reporters.csv"), stringsAsFactors = FALSE, header = TRUE)
+partners <- read.csv(here("data", "partners.csv"), stringsAsFactors = FALSE, header = TRUE)
 hscodes <- read.csv(here("data", "hscodes.csv"), stringsAsFactors = FALSE, header = TRUE)
 
 hscodes <- hscodes %>% 
@@ -58,3 +59,24 @@ cht <- ggplot(mktshare, aes(x = year, y = mktshare, fill = partner)) +
         scale_x_discrete(year)
 
 cht
+
+
+sankey <- test %>% 
+          filter(partner_iso != "WLD") %>% 
+          select(reporter_iso,
+                 partner_iso,
+                 trade_value_us = trade_value_us_) %>% 
+          arrange(desc(trade_value_us)) %>% 
+          mutate(source = 0, target =seq.int(nrow(sankey)), value = trade_value_us)
+
+nodes <- as.vector(sankey$partner_iso)
+nodes <- append("CHN", nodes)
+nodes <- data.frame("name" = nodes)
+
+links <- sankey %>% 
+        select(source, target, value) 
+
+sankeyNetwork(Links = links, Nodes = nodes,
+              Source = "target", Target = "source",
+              Value = "value", NodeID = "name",
+              fontSize= 12, nodeWidth = 30)

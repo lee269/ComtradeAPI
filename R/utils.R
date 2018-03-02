2b1dd4360e3291b716c0c91f0a6e248cc48307a4
David Lee <david@lee269.co.uk>
2018-03-02 17:23:25 +0000

    dashboard2

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,26 @@ dbSafeNames = function(names) {
   names = gsub('.','_',names, fixed=TRUE)
   names
 }
+
+
+# http://stackoverflow.com/questions/28159936/formatting-large-currency-or-dollar-values-to-millions-billions
+money <-  function(x) {
+  z <- abs(x)
+  
+  if (is.na(z)) {return(0)}
+  
+  b.index <-  z >= 1e9
+  m.index <-  z >= 1e5 & z < 1e9
+  
+  if (x > 0) {
+    output <-  paste("£", formatC(z, format = "d", big.mark = ","), sep = "")
+    output[b.index] <-  paste("£", formatC(z[b.index] / 1e9, digits = 1, format = "f"), "bn", sep = "")
+    output[m.index] <-  paste("£", formatC(x[m.index] / 1e6, digits = 1, format = "f"), "m", sep = "")
+    return(output)
+  } else {
+    output <-  paste("-£", formatC(z, format = "d", big.mark = ","), sep = "")
+    output[b.index] <-  paste("-£", formatC(z[b.index] / 1e9, digits = 1, format = "f"), "bn", sep = "")
+    output[m.index] <-  paste("-£", formatC(z[m.index] / 1e6, digits = 1, format = "f"), "m", sep = "")
+    return(output)
+  }
+}
